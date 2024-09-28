48296654cafbd7dbf274eea0d25cccd093e2c40c
birajit-maaze <birajit@falconsystem.com>
2024-09-28 11:19:49 +0530

    serializers for login done

--- users/serializers.py
+++ users/serializers.py
@@ -63,4 +63,11 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
     
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=50)
+    password = serializers.CharField(max_length=50)
+
+    
     
